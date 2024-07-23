a7a9e29a6742eba23b2eac73caf46d4f095bca15
amateur-de <danialcs606@gmail.com>
2024-07-23 21:52:19 +0500

    Added color changing effect when cursor passes through grid squares

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const squareColor = (e)=>{
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${red} , ${green} , ${blue})`
+
+}
+
+
+
 let squares = 16
 const container = document.querySelector('.container')
 for(let rows = 0; rows < squares ; rows = rows + 1)
@@ -7,6 +17,7 @@ for(let rows = 0; rows < squares ; rows = rows + 1)
     {
         const square = document.createElement('div')
         square.classList.add('column')
+        square.addEventListener('mouseover' , squareColor)
         row.appendChild(square)
 
     }
