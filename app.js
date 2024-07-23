6e0e8f54709904464d6c64a9ef56a66bd9dcc2c6
amateur-de <danialcs606@gmail.com>
2024-07-23 22:50:39 +0500

    Added an effect through which a square becomes permanently dark after 10 interactions with cursor

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const squareColor = (e)=>{
     const red = Math.floor(Math.random() * 256)
     const green = Math.floor(Math.random() * 256)
     const blue = Math.floor(Math.random() * 256)
+    if(e.target.style.opacity === ``)
+    {
+        e.target.style.opacity = `1`
+    }
+    else if(Number(e.target.style.opacity) > 0)
+    {
+        e.target.style.opacity = (Number(e.target.style.opacity) - 0.1).toString()
+    }
+    /*
+    console.log(e.target.style.opacity)
+    if(Number(e.target.style.opacity) > 0)
+    {
+        e.target.style.opacity = (Number(e.target.style.opacity) - 0.1).toString()
+    }
+        */
+    console.log(e.target.style.opacity)
     e.target.style.backgroundColor = `rgb(${red} , ${green} , ${blue})`
 
 }
@@ -16,6 +32,7 @@ for(let rows = 0; rows < squares ; rows = rows + 1)
     for(let cols =0; cols < squares; cols = cols + 1)
     {
         const square = document.createElement('div')
+        square.style.backgroundColor = 'white'
         square.classList.add('column')
         square.addEventListener('mouseover' , squareColor)
         row.appendChild(square)
